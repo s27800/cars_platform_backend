ebb8b303fc1889a0e102058a560d040e139e1b52
s27800 <s27800@pjwstk.edu.pl>
2025-10-13 18:50:34 +0200

    Initial commit: Add configuration

---
+++ src/main/java/com/carsplatform/backend/CarsPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package com.carsplatform.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsPlatformBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarsPlatformBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/carsplatform/backend/CarsPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carsplatform.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsPlatformBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
