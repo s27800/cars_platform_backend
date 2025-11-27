51b7e0ae3109921dd3e35dc985d295d57c591e39
s27800 <s27800@pjwstk.edu.pl>
2025-11-27 16:14:28 +0100

    Created: - CarFuelReportResponse - CarReviewResponse - UsernameResponse to allow sending fuel consumptions and reviews information.

---
+++ src/main/java/com/carsplatform/backend/api/reviews/dtos/CarReviewResponse.java
@@ -0,0 +1,33 @@
+package com.carsplatform.backend.api.reviews.dtos;
+
+import com.carsplatform.backend.api.users.dtos.UsernameResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CarReviewResponse {
+    private Long id;
+    private String comment;
+    private Integer engineRating;
+    private Integer transmissionRating;
+    private Integer steeringRating;
+    private Integer suspensionRating;
+    private Integer visibilityRating;
+    private Integer ergonomicsRating;
+    private Integer soundProofingRating;
+    private Integer interiorSpaceRating;
+    private Integer maintenanceRating;
+    private Integer priceQualityRating;
+    private Integer failureFreeRating;
+    private LocalDateTime reviewDate;
+    private Boolean isApproved;
+    private UsernameResponse usernameResponse;
+}
---
+++ src/main/java/com/carsplatform/backend/api/users/dtos/UsernameResponse.java
@@ -0,0 +1,14 @@
+package com.carsplatform.backend.api.users.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsernameResponse {
+    private String username;
+}
