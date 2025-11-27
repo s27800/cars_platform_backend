f569ccfa67a9197bd0765aaba7d622a9a36f85c0
s27800 <s27800@pjwstk.edu.pl>
2025-11-27 22:15:01 +0100

    fix: - added map methods to BrandMapper and ModelMapper

--- src/main/java/com/carsplatform/backend/api/brands/BrandMapper.java
+++ src/main/java/com/carsplatform/backend/api/brands/BrandMapper.java
@@ -2,11 +2,22 @@ package com.carsplatform.backend.api.brands;
 
 import com.carsplatform.backend.api.brands.dtos.CarBrandResponse;
 
+import com.carsplatform.backend.api.cars.Car;
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface BrandMapper {
     BrandMapper INSTANCE = Mappers.getMapper(BrandMapper.class);
+
     CarBrandResponse toDto(Brand brand);
+
+    @Named("map")
+    default CarBrandResponse map(Car car) {
+        if (car == null)
+            return null;
+
+        return toDto(car.getGeneration().getModel().getBrand());
+    }
 }
