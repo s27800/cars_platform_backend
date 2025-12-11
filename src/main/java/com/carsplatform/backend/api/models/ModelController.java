975d8368bc7ab89ea503c6b6aaaeeabc143485fd
s27800 <s27800@pjwstk.edu.pl>
2025-12-11 16:34:55 +0100

    Features: - get model details with generations
    Created:
    - ModelController
    - ModelService
    - ModelDetailsResponse
    - IModelRepository
    - IModelDetailsMapper
    - GenerationsListResponse
    - IGenerationsListMapper

---
+++ src/main/java/com/carsplatform/backend/api/models/ModelController.java
@@ -0,0 +1,33 @@
+package com.carsplatform.backend.api.models;
+
+import com.carsplatform.backend.api.models.dtos.ModelDetailsResponse;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/models")
+@RequiredArgsConstructor
+@Tag(name = "Models", description = "API for managing car models")
+@SecurityRequirement(name = "bearerAuth")
+public class ModelController {
+
+    private final ModelService service;
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get model details with a list of generations")
+    public ResponseEntity<ModelDetailsResponse> getModelsByBrandId(
+            @Parameter(description = "ID of the model") @PathVariable Integer id) {
+
+        ModelDetailsResponse model = service.getModelDetailsById(id);
+
+        return ResponseEntity.ok(model);
+    }
+}
