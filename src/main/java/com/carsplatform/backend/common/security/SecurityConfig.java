faf2a2b9283171e7a3ee2071d86f2a8959497b00
s27800 <s27800@pjwstk.edu.pl>
2026-05-30 21:13:27 +0200

    Fix: - update CORS configuration to allow localhost:5173

--- src/main/java/com/carsplatform/backend/common/security/SecurityConfig.java
+++ src/main/java/com/carsplatform/backend/common/security/SecurityConfig.java
@@ -98,7 +98,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:5173"));
 
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
